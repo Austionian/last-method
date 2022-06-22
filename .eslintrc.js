f0c62dfa66c5d2b2a2aaf121461ce707a3b88bba
Austin Rooks <Austionian@users.noreply.github.com>
2022-06-22 10:09:08 -0500

    configure linting

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true,
+        'node': true
+    },
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        'semi': ['error', 'never'],
+        'quotes': ['error', 'single']
+    }
+}
--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,11 +1,11 @@
 const index = require('../index')
 
-test("Returns the last element of an array", () => {
+test('Returns the last element of an array', () => {
     const arr = [1, 2, 3, 4, 5]
-    expect(arr.last()).toBe(5);
-});
+    expect(arr.last()).toBe(5)
+})
 
-test("Returns -1 in an empty array", () => {
+test('Returns -1 in an empty array', () => {
     const arr = []
     expect(arr.last()).toBe(-1)
 })
