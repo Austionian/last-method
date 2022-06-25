d5b6fc00ae9f02b0753011e6c6dbf90ade7f70f6
Austin Rooks <Austionian@users.noreply.github.com>
2022-06-25 10:33:49 -0500

    update docs

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ Function adds last() to all arrays in which this
 module is imported to. Last() will return the 
 last value in the array. If the array is empty
 it will return -1.
+
+n.b. it's not really a good idea to actually use
+this and this repo was created in jest.
 */
 Array.prototype.last = function() {
     if (this.length > 0) {
